3c338e49173f922814150243c65d749642719891
ramonsancheez <rsanchezabadia@cifpfbmoll.eu>
2023-05-21 16:49:55 +0000

    refactorizando rutas

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', [ProductController::class, 'index'])->name('product.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/products', [ProductController::class, 'store'])->name('product.store');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('product.show');
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::patch('/products/{product}', [ProductController::class, 'update'])->name('product.update');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
-
+Route::resource('products', ProductController::class, [
+    'names' => 'product',
+    'parameters' => ['products' => 'product']
+]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
