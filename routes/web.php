3936418c1ce6e01bc95fef840b9edf003be8e0fd
ramonsancheez <rsanchezabadia@cifpfbmoll.eu>
2023-05-23 18:30:54 +0000

    redirijiendo a /products al hacer login

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::resource('products', ProductController::class, [
     'names' => 'product',
     'parameters' => ['products' => 'product']
