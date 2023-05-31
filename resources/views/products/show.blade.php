455764487f5ae8ef936f952e1ebdb71f338ff989
ramonsancheez <rsanchezabadia@cifpfbmoll.eu>
2023-05-31 17:04:13 +0000

    condicionales según si eres owner o buyer

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -7,17 +7,30 @@
 
 <body id="product" class="product">
     <header class="main-header">
-        <h1 class="main-header__title">eRRRe</h1>
+        <a href="/products">
+            <h1 class="main-header__title">eRRRe</h1>
+            <h3 class="main-header__subtitle">Recycle, reduce, reuse</h3>
+        </a>
         <nav>
             <ul class="main-header__nav">
-                <li>
-                    <a class="main-header__nav__link" href="{{ route('product.index') }}">Productos</a>
+                <li class="main-header__nav__item">
+                    <a id="link1" class="main-header__nav__link" href="{{ route('product.index') }}">Productos</a>
+                </li>
+                <li class="main-header__nav__item">
+                    <a id="link2" class="main-header__nav__link" href="{{ route('product.create') }}">Subir Producto</a>
                 </li>
                 <li>
-                    <a class="main-header__nav__link" href="{{ route('product.create') }}">Subir Producto</a>
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Log Out
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </li>
             </ul>
         </nav>
+        
     </header>
     <main class="product-content">
         <div class="product-container">
@@ -32,19 +45,27 @@
                 <hr>
                 <div class="product__info__description">{{$product->description}}</div>
                 <div class="product__info__toDo">
-                    @if ($product->user_id == Auth::user()->id)
-                        <div class="product__info__edit"><a href="{{route('product.edit', $product)}}">Editar producto</a></div>
+                    @if ($product->user_id != Auth::user()->id && $product->buyer_id == Auth::user()->id)
+                        <div class="product__info__sold">Comprado a: {{ $product->user_id}}</div>
+                    @elseif ($product->buyer_id != 0 && $product->user_id == Auth::user()->id)
+                        <div class="product__info__sold">Vendido a: {{ $product->buyer_id }}</div>
+                    @elseif ($product->user_id == Auth::user()->id && $product->buyer_id == 0)
+                        <div class="product__info__edit"><a href="{{ route('product.edit', $product) }}">Editar producto</a></div>
                         <div class="product__info__delete">
-                            <form action="{{route('product.destroy', $product)}}" method="POST">
+                            <form action="{{ route('product.destroy', $product) }}" method="POST">
                                 @csrf
                                 @method('delete')
                                 <button type="submit">Eliminar</button>
                             </form>
                         </div>
                     @else
-                        <div class="product__info__buy"><a href="#0">Comprar</a></div>
+                        <form action="{{ route('product.purchase', $product) }}" method="POST">
+                            @csrf
+                            <button type="submit">Comprar producto</button>
+                        </form>
                     @endif
                 </div>
+                
             </div>
         </div>
         
